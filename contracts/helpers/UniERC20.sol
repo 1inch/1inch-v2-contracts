bdbda2c74b772e99687200f487f687cc96e5a26d
Mikhail Melnik <by.zumzoom@gmail.com>
2020-12-07 14:16:41 +0300

    optimise uniApprove

--- contracts/helpers/UniERC20.sol
+++ contracts/helpers/UniERC20.sol
@@ -4,12 +4,10 @@ pragma solidity ^0.6.12;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 
 library UniERC20 {
     using SafeMath for uint256;
-    using SafeERC20 for IERC20;
 
     IERC20 private constant _ETH_ADDRESS = IERC20(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
     IERC20 private constant _ZERO_ADDRESS = IERC20(0);
@@ -31,7 +29,7 @@ library UniERC20 {
             if (isETH(token)) {
                 to.transfer(amount);
             } else {
-                token.safeTransfer(to, amount);
+                _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, amount));
             }
         }
     }
@@ -39,16 +37,22 @@ library UniERC20 {
     function uniApprove(IERC20 token, address to, uint256 amount) internal {
         require(!isETH(token), "Approve called on ETH");
 
-        if (amount == 0) {
-            token.safeApprove(to, 0);
-        } else {
-            uint256 allowance = token.allowance(address(this), to);
-            if (allowance < amount) {
-                if (allowance > 0) {
-                    token.safeApprove(to, 0);
-                }
-                token.safeApprove(to, amount);
-            }
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = address(token).call(abi.encodeWithSelector(token.approve.selector, to, amount));
+
+        if (!success || (returndata.length > 0 && !abi.decode(returndata, (bool)))) {
+            _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, to, 0));
+            _callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, to, amount));
+        }
+    }
+
+    function _callOptionalReturn(IERC20 token, bytes memory data) private {
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = address(token).call(data);
+        require(success, "low-level call failed");
+
+        if (returndata.length > 0) { // Return data is optional
+            require(abi.decode(returndata, (bool)), "ERC20 operation did not succeed");
         }
     }
 }
